d84f62c4421ca3a4806900a5690d10238e3a2be8
AleksandrMuratov <zimakip@yandex.ru>
2022-05-10 22:52:02 +0800

    final project sprint 4

---
+++ search-server/document.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <iostream>
+
+struct Document {
+    Document();
+
+    Document(int id, double relevance, int rating);
+
+    int id;
+    double relevance;
+    int rating;
+};
+
+std::ostream& operator<<(std::ostream& out, const Document& document);
