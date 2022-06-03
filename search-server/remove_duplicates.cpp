7c7dc94a11108ce1e2d96e23a1447938a0e17bb5
AleksandrMuratov <zimakip@yandex.ru>
2022-06-03 19:37:18 +0800

    final project sprint 5 fixed

--- search-server/remove_duplicates.cpp
+++ search-server/remove_duplicates.cpp
@@ -5,16 +5,15 @@
 #include "remove_duplicates.h"
 
 void RemoveDuplicates(SearchServer& search_server) {
-	std::set<std::string> words_of_documents_concatenation;
+	std::set<std::set<std::string>> words_of_documents;
 	std::vector<int> document_id_to_erase;
 	for(int document_id : search_server) {
 		const std::map<std::string, double>&  words_freq = search_server.GetWordFrequencies(document_id);
-		std::string text;
+		std::set<std::string> words_of_document;
 		for (const auto& [word, freq] : words_freq) {
-			text += ' ';
-			text += word;
+			words_of_document.insert(word);
 		}
-		if (!words_of_documents_concatenation.insert(text).second) {
+		if (!words_of_documents.insert(words_of_document).second) {
 			std::cout << "Found duplicate document id " << document_id << '\n';
 			document_id_to_erase.push_back(document_id);
 		}
